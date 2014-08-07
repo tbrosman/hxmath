afae53845bb5f2ecf63db40844eb1bfccd020008
Tommy A. Brosman IV <tommy.brosman@gmail.com>
2014-08-06 23:19:53 -0700

    Added copy* functions to converters and changed them to use a more rigorous naming scheme to differentiate clone* and copy* operations.

--- test/TestConverters.hx
+++ test/TestConverters.hx
@@ -2,6 +2,7 @@ package test;
 
 import hxmath.converters.Vector2Converter;
 import hxmath.Vector2;
+import test.TestConverters.FlxPointMock;
     
 class FlxPointMock
 {
@@ -45,12 +46,22 @@ class TestConverters extends MathTestCase
     {
         var v = new Vector2(3.0, 1.0);
         
-        var p:FlxPointMock = Vector2Converter.to(v);
+        var p:FlxPointMock = Vector2Converter.cloneTo(v);
         assertEquals(v.x, p.x);
         assertEquals(v.y, p.y);
         
-        var u:Vector2 = Vector2Converter.fromFlxPoint(p);
+        var u:Vector2 = Vector2Converter.cloneFromFlxPoint(p);
         assertEquals(u.x, p.x);
         assertEquals(u.y, p.y);
+        
+        var q = new FlxPointMock();
+        Vector2Converter.copyToFlxPoint(v, q);
+        assertEquals(v.x, q.x);
+        assertEquals(v.y, q.y);
+        
+        var u = new Vector2();
+        Vector2Converter.copyFromFlxPoint(u, q);
+        assertEquals(u.x, q.x);
+        assertEquals(u.y, q.y);
     }
 }
