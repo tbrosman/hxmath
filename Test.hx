fc10ad73d674848b1c507e72ebfe72d37bcdc2de
Tommy A. Brosman IV <tommy.brosman@gmail.com>
2014-08-13 22:44:18 -0700

    Fixed the test cases. Should close issue #1.

--- Test.hx
+++ Test.hx
@@ -2,12 +2,12 @@ import test.TestMain;
 
 class Test {
     static function main() {
-        var failed = TestMain.main();
+        var passed = TestMain.main();
         
         #if (sys && !EXIT_ON_FINISH)
         Sys.stdin().readLine();
         #else
-        Sys.exit(failed ? -1 : 0);
+        Sys.exit(passed ? 0 : -1);
         #end
     }
 }
